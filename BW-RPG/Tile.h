85f40b2ee606c7ff23e9a1298fc752f9d1b4b2d2
Jwasik <jj.wasik@wp.pl>
2020-02-22 15:33:41 +0100

    Tile update

--- BW-RPG/Tile.h
+++ BW-RPG/Tile.h
@@ -4,8 +4,13 @@
 class Tile : public Drawable
 {
 public:
-
+	Tile();
+	Tile(int,std::string);
+	bool loadTexture(std::string);
 private:
-
+	bool canWalkOn = 1;
+	int ID = 0;
+	std::string name;
+	sf::Texture tileTexture;
 };
 
